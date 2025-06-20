77bbcc5d2532d15cd7477e5a5d48e9ceca368a4b
imc <ivan.cenov@rnd.bg>
2025-06-20 16:53:18 +0300

    Using iot button, not finished

--- main/proc.c
+++ main/proc.c
@@ -8,9 +8,14 @@
 
 #include "freertos/FreeRTOS.h"
 #include "esp_log.h"
+#include "driver/gpio.h"
+#include "iot_button.h"
+#include "button_gpio.h"
 
 #include "commondefs.h"
 #include "anvs.h"
+
+// static const char TAG[] = "proc";
 
 typedef struct {
     device_modes_t opmode;  // actual operative mode
@@ -45,4 +50,27 @@ esp_err_t read_opmode(void)
     esp_err_t ret = anvs_app_op_mode_get((uint16_t* )&operative_state.opmode);
     portEXIT_CRITICAL(&operative_state.mux);
     return ret;
+}
+
+// button handling
+
+#define BUTTON_GPIO 12
+#define BUTTON_ACTIVE_LEVEL 0
+
+button_config_t btn_cfg = {0};
+
+button_gpio_config_t gpio_cfg = {
+    .gpio_num = BUTTON_GPIO,
+    .active_level = BUTTON_ACTIVE_LEVEL,
+    .enable_power_save = false,
+};
+
+button_handle_t btn;
+
+
+void init_button(void)
+{
+    esp_err_t ret = iot_button_new_gpio_device(&btn_cfg, &gpio_cfg, &btn);
+
+    iot_button_register_cb(btn, BUTTON_SINGLE_CLICK, NULL, button_event_cb, NULL);
 }
